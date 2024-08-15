f30516fa22583016c40485a4167b93c4934fe88a
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2024-08-15 13:38:49 +0200

    Update scrollTo.js

--- src/scrollTo.js
+++ src/scrollTo.js
@@ -3,30 +3,32 @@
 // <div id="targetId"></div>
 
 const defaults = {
-  offset: 0,
+    offset: 0,
+    preventDefault: 1,
 }
 
 /**
  * Initializes smooth scrolling to target
  */
 export default function initScrollTo() {
-  const components = document.querySelectorAll("[data-scroll-target]")
+    const components = document.querySelectorAll("[data-scroll-target]")
 
-  components.forEach((component) => {
-    const { scrollTarget, scrollOffset } = component.dataset
-    const targetElement = document.getElementById(scrollTarget)
-    const offset = typeof scrollOffset === "undefined" ? defaults.offset : parseInt(scrollOffset)
+    components.forEach((component) => {
+        const { scrollTarget, scrollOffset, scrollPreventDefault } = component.dataset
+        const targetElement = document.getElementById(scrollTarget)
+        const offset = typeof scrollOffset === "undefined" ? defaults.offset : parseInt(scrollOffset)
+        const preventDefault = typeof scrollPreventDefault === "undefined" ? defaults.preventDefault : parseInt(scrollPreventDefault)
 
-    component.addEventListener("click", (event) => {
-      // disables <a> tag from redirecting
-      if (targetElement) {
-        event.preventDefault()
-      }
+        component.addEventListener("click", (event) => {
+            // disables <a> tag from redirecting
+            if (targetElement && preventDefault) {
+                event.preventDefault()
+            }
 
-      window.scrollTo({
-        top: targetElement.getBoundingClientRect().top + window.scrollY + offset,
-        behavior: "smooth",
-      })
+            window.scrollTo({
+                top: targetElement.getBoundingClientRect().top + window.scrollY + offset,
+                behavior: "smooth",
+            })
+        })
     })
-  })
 }
