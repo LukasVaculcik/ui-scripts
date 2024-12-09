9212738581e0c712d1fa9a9dfdce1c2ca2da6fcf
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2024-12-09 15:44:41 +0100

    Update fade.js
    remove semicolons

--- src/fade.js
+++ src/fade.js
@@ -3,15 +3,15 @@
  * @param {HTMLElement} element
  */
 export function fadeOut(element) {
-  element.style.opacity = 1;
+  element.style.opacity = 1
   function fade() {
     if ((element.style.opacity -= 0.1) < 0) {
-      element.style.display = "none";
+      element.style.display = "none"
     } else {
-      requestAnimationFrame(fade);
+      requestAnimationFrame(fade)
     }
   }
-  fade();
+  fade()
 }
 
 /**
@@ -20,14 +20,14 @@ export function fadeOut(element) {
  * @param {string} display
  */
 export function fadeIn(element, display) {
-  element.style.opacity = 0;
-  element.style.display = display || "block";
+  element.style.opacity = 0
+  element.style.display = display || "block"
   function fade() {
-    let val = parseFloat(element.style.opacity);
+    let val = parseFloat(element.style.opacity)
     if (!((val += 0.1) > 1)) {
-      element.style.opacity = val;
-      requestAnimationFrame(fade);
+      element.style.opacity = val
+      requestAnimationFrame(fade)
     }
   }
-  fade();
+  fade()
 }
